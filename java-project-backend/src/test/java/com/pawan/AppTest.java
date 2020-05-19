ae3c5dd9b0119f11575a6adf015294a60cd1b5f6
Pawan choudhary <>
2020-05-19 23:46:32 +0530

    Test

---
+++ java-project-backend/src/test/java/com/pawan/AppTest.java
@@ -0,0 +1,12 @@
+package com.pawan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
